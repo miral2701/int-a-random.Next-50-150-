18c5c29d14759388418b206dda4cbc24d773b505
Mira Madani <miramadani2701@gmail.com>
2024-05-07 22:48:58 +0300

    Add project files.

---
+++ GraphicHW/Form1.cs
@@ -0,0 +1,33 @@
+namespace GraphicHW
+{
+    public partial class Form1 : Form
+    {
+        Point p;
+        Graphics g;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            g = CreateGraphics();
+        }
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            p.X = e.X;
+            p.Y = e.Y;
+            Random random= new Random();    
+            int a=random.Next(50,150);
+            int b = random.Next(50, 150);
+
+            int R = random.Next(0, 255);
+            int G = random.Next(0, 255);
+            int B = random.Next(0, 255);
+
+            Brush brush = new SolidBrush(Color.FromArgb(255,R, G, B));
+            g.FillEllipse(brush, p.X, p.Y, a, b);
+        }
+
+       
+    }
+}
